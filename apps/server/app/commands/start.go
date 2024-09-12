59df055194f149dd669efcb9f1021af603c4d5c9
Vyacheslav Tilikov <vyutilikov@avito.ru>
2024-09-12 13:21:16 +0300

    small refactoring

--- apps/server/app/commands/start.go
+++ apps/server/app/commands/start.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/spf13/cobra"
 
-	"github.com/Isotere/kw-database/apps/server/app/handlers/start"
+	"github.com/Isotere/kw-database/apps/server/app/commands/start"
 )
 
 // startCmd represents the start command
